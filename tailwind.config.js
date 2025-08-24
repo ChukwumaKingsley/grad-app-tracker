bf35623facda6e7a11a2129f2cca1b985e688423
chukwumakingsley <chukwuma.c.kingsley@gmail.com>
2025-08-24 07:19:07 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3A8A', // Navy Blue
+        secondary: '#3B82F6', // Sky Blue
+        neutralLight: '#F3F4F6', // Light Gray
+        neutralDark: '#374151', // Dark Gray
+        accent: '#10B981', // Emerald Green
+      },
+    },
+  },
+  plugins: [],
+}
